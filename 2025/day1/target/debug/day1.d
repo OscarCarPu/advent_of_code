0013b7f9e965fa1a743252ba6f84f315f7356054
OCP <oscarcarballopuebla@gmail.com>
2025-12-01 12:05:42 +0100

    target dleeted

--- 2025/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/ocp/dev/advent_of_code/2025/day1/target/debug/day1: /home/ocp/dev/advent_of_code/2025/day1/src/main.rs
--- 2025/day1/target/debug/second-star.d
+++
@@ -1,1 +0,0 @@
-/home/ocp/dev/advent_of_code/2025/day1/target/debug/second-star: /home/ocp/dev/advent_of_code/2025/day1/src/bin/second-star.rs
